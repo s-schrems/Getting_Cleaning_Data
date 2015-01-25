57075d9a1092421d7fda4b96335043b53aad8ad5
sschrems <sschrems@gmail.com>
2015-01-25 14:12:03 -0500

    uploading run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,18 @@
+setwd("~/Data_Science_Trek/Classes/3_Getting_and_Cleaning Data/Course_Project")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "dataset.zip", method = "curl")
+unzip("dataset.zip")
+
+#read in testing data and combine data
+x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+
+#read in training data and combine data
+x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+
+
